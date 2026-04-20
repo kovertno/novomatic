91e0f1f5469143b75963fba8a000f078c27e7249
konrad kurzak <konradkurzak@konrads-Laptop.local>
2026-04-20 22:47:55 +0200

    Initial commit - all 3 tasks

--- task1/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/konradkurzak/DIRS/novomatic/task1
-# Build directory: /Users/konradkurzak/DIRS/novomatic/task1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Task1Tests "/Users/konradkurzak/DIRS/novomatic/task1/build/Task1Tests")
-set_tests_properties(Task1Tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/konradkurzak/DIRS/novomatic/task1/CMakeLists.txt;30;add_test;/Users/konradkurzak/DIRS/novomatic/task1/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- task2/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/konradkurzak/DIRS/novomatic/task2/build/_deps/googletest-src
-# Build directory: /Users/konradkurzak/DIRS/novomatic/task2/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
